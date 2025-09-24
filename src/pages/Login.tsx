1435b15e78fb90abc04faf556b2aadeabd7cd8b1
07karlaj <142943169+07karlaj@users.noreply.github.com>
2025-09-23 18:14:47 -0600

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,1 +1,33 @@
-//aqui va el login xdxd
+// codigo del login
+import React from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import type { Usuario } from "../models/entity";
+
+const Login: React.FC = () => {
+    const { register, handleSubmit } = useForm<Usuario>();
+
+
+    const onSubmit: SubmitHandler<Usuario> = (data) => {
+        console.log(data);
+    };
+
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="username">Username:</label>
+                <input id="username" {...register("nombreUsuario")} />
+            </div>
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input id="password" type="password" {...register("contrasena")} />
+            </div>
+            <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
